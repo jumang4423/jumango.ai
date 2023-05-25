40cd54f5bba47b8fccfe63fc36a085ab85da7f4b
jumang4423 <catontheskate4423@gmail.com>
2023-05-26 02:40:11 +0900

    refac

--- src/ai_chat/chat.py
+++ src/ai_chat/chat.py
@@ -9,17 +9,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # states
 RECALL_MAX = 2
-SYSTEM_PROMPT = "you are a jumango, creative nerd hacker. you answers always very shortly."
+SYSTEM_PROMPT = "you are jumango.ai, creative nerd hacker bot. you answers always very shortly based on recall memory."
 
 def chat_from_global_conv_his(conv_his, prompt):
     with_system = [{ 'role': "system", 'content': SYSTEM_PROMPT }]
     recalls = recall(prompt, RECALL_MAX)
     for r in recalls:
-        with_system.append({ 'role': "assistant", 'content': r })
+        with_system.append({ 'role': "system", 'content': "recall: {0}".format(r) })
     for history in conv_his:
         with_system.append(history)
-
-    with_system.append({ 'role': "user", 'content': prompt })
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=with_system
@@ -27,3 +25,8 @@ def chat_from_global_conv_his(conv_his, prompt):
     ai_response = response.choices[0].message.content
 
     return ai_response
+
+def is_msg_spam(msg):
+    response = openai.Moderation.create(input=msg)
+
+    return response['results'][0]['flagged']
--- src/discord_impl/api.py
+++ src/discord_impl/api.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import discord
 import os
 import random
-from src.ai_chat.chat import chat_from_global_conv_his
+from src.ai_chat.chat import chat_from_global_conv_his,  is_msg_spam
 from src.chroma.db import remember, recall
 
 # env load
@@ -16,17 +16,19 @@ bot_emoji_exp = {
     "chat": "🫧",
     "memorize": "✨",
     "erase": "💨",
-    "thinking": "🤔"
+    "thinking": "🤔",
+    "spam": "🚫",
 }
 
 def help():
     return """```
 jumango.ai help
 commands:
-  (echo) repeat what you say
-  (<)    chat with jumango
-  (<<)   remember what you say
-  (*)    forget previous memory
+    ()     chat with jumango
+    (<<)   remember what you say
+    (*)    forget previous memory
+    (echo) repeat what you say
+    (help) show this message
 ```"""
 
 
@@ -43,12 +45,16 @@ def random_exp():
 
     return exps[random.randint(0, len(exps) - 1)]
 
-class MyClient(discord.Client):
+def mention_str(user_id):
+    return "<@{0}>".format(user_id)
+
+class JumangoAIBot(discord.Client):
     def mention_str(self):
-        return "<@{0}>".format(self.user.id)
+        return mention_str(self.user.id)
 
     def add_conv_his(self, msg, userName, is_msg_from_bot):
         global conv_his
+        MAX_WORD_LEN = 64
         if is_msg_from_bot and msg[:1] != bot_emoji_exp["chat"]:
             return
         if is_msg_from_bot:
@@ -60,12 +66,17 @@ class MyClient(discord.Client):
                 msg_arr = msg_arr[1:]
                 if len(msg_arr) == 0:
                     msg_arr = ["how do you think?"]
+            # if msg is too long, split it
+            if len(msg_arr) > MAX_WORD_LEN:
+                msg_arr = msg_arr[:MAX_WORD_LEN]
             conv_his.append({ 'role': 'user', 'content': "{0}: {1}".format(userName, ' '.join(msg_arr)) })
             if len(conv_his) > MAX_LEN:
                 conv_his.pop(0)
 
-        print("conv_his: ", conv_his)
-
+    async def send_alert_msg(self, msg, userID):
+        channel_id = 1109401858348425256
+        channel = self.get_channel(channel_id)
+        await channel.send("{0} spam detected: {1} {2}".format(bot_emoji_exp["spam"], mention_str(userID), msg))
 
     async def on_ready(self):
         print('logged on as {0}'.format(self.user))
@@ -73,6 +84,10 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         is_msg_from_bot = message.author == self.user
         userName = message.author.name
+        is_spam = is_msg_spam(message.content)
+        if is_spam and not is_msg_from_bot:
+            # if message is spam, notify to # logs channel
+            await self.send_alert_msg(message.content, message.author.id)
         self.add_conv_his(message.content, userName, is_msg_from_bot)
         if is_msg_from_bot:
             return
@@ -81,8 +96,7 @@ class MyClient(discord.Client):
             return
         command = commands[1] if len(commands) > 1 else ""
         args = commands[2:] if len(commands) > 2 else []
-        print('command from {0.author}: {0.content}'.format(message))
-
+        print('ai trrigered: {0}'.format(message.content[1:]))
         if command == "echo":
             await message.channel.send(' '.join(args))
         elif command == "<<":
@@ -95,12 +109,11 @@ class MyClient(discord.Client):
             await message.channel.send(help())
         else:
             await message.channel.send("{0} {1}".format(bot_emoji_exp["thinking"], random_exp()))
-            prompt = "hi jumango, " + ' '.join(commands[1:]) if len(command) != 0 else "how do you think?"
-            print("prompt: ", prompt)
+            prompt = "hi jumango.ai, " + ' '.join(commands[1:]) if len(command) != 0 else "how do you think?"
             bot_response = chat_from_global_conv_his(conv_his, prompt)
             await message.channel.send("{0} {1}".format(bot_emoji_exp["chat"], bot_response))
 
 # init bot
 intents = discord.Intents.default()
 intents.message_content = True
-client = MyClient(intents=intents)
+client = JumangoAIBot(intents=intents)
